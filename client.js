86c31aa99f763adcb10498413ad3709f064b94c7
Austin Bourdier <austinbourdier@gmail.com>
2014-09-13 14:02:30 -0700

    geolocation doesn't work on iphone safari. works on mobile chrome, will revisit later

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position){success(position)}, failure())
+  navigator.geolocation.getCurrentPosition(success, failure)
 } else {
   alert('error')
 }
 
 function success(position){
+  console.log(position)
   alert('success')
 }
 
