c92298f8e2248dad7cb5372d463382adbf1ef8dd
Austin Bourdier <austinbourdier@gmail.com>
2014-09-13 14:14:54 -0700

    adding in body parser for location data info

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app     = express();
+var bodyParser = require('body-parser')
 var server  = require('http').createServer(app);
 var _ = require('underscore-node');
 var geolib = require('geolib');
 var Factual = require('factual-api');
 var factual = new Factual('r87lYmE5RDLMQOOcETtoZqnLYEvurbTBfUIUAt8y', '6HZGifE7NYj85JvcKnAqQQ0UKn7gYgFzn7oroQGH');
 console.log("Node server started on port 5000");
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded()); // to support URL-encoded bodies
 
 app.use('/', express.static(__dirname + '/'));
 app.get('/', function(req,res) {
@@ -13,6 +16,10 @@ app.get('/', function(req,res) {
   res.sendfile(__dirname + '/index.html');
   search = req.query || "";
 });
+
+app.post('/', fucntion(req, res){
+  console.log(req)
+})
 
 factual.get('/t/places-us', {geo:{"$circle":{"$center":[34.058583, -118.416582],"$meters":1000}}}, function (error, res) {
   // console.log(res.data);
